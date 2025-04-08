832bc5feed9ba4ea4f11db388bd949b94619f626
adohong4 <adohong4@gmail.com>
2025-04-08 23:59:05 +0700

    Menu Drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:minimal_chat_app/auth/auth_service.dart';
+import 'package:minimal_chat_app/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void logout() {
-    final _auth = AuthService();
-    _auth.signOut();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        actions: [IconButton(onPressed: logout, icon: Icon(Icons.logout))],
-      ),
-      drawer: Drawer(),
+      appBar: AppBar(title: const Text('Home Page')),
+      drawer: MyDrawer(),
     );
   }
 }
